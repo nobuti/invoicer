a1a689a9389cb63a8cc6f09deb0f0095f5121866
nobuti <buti@nobuti.com>
2013-04-28 20:48:54 +0200

    Added some initial data in migrations.

--- migrations/007_insert_base_clients.rb
+++ migrations/007_insert_base_clients.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   up do
-    self[:clients].insert(:name => 'Nicemondays SCP', :cif => 'J92682345', :address => 'Cno. Antiguo Paso del Ferrocarril Edificio Horizontes, 2 10aA 29640 Fuengirola (Málaga)', :created_at => Time.now)
+    self[:clients].insert(:name => 'Nicemondays SCP', :cif => 'J92682345', :address => 'Cno. Antiguo Paso del Ferrocarril Edificio Horizontes, 2 10aA 29640 Fuengirola (Malaga)', :created_at => Time.now)
     self[:clients].insert(:name => 'BeBanjoSL', :cif => 'B85450864', :address => 'Paseo de la Castellana, 178 1o Izq. 28046 Madrid Spain', :created_at => Time.now)
   end
   down do
